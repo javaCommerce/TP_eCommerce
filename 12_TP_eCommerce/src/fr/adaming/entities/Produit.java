dfc3d0f5ddee23bc24cc76401414449f70b9b35d
inti0489 <inti0489@192.168.1.57>
2018-08-08 21:11:46 +0200

    jd : commit de la classe produit et du persistence.xml

--- 12_TP_eCommerce/src/fr/adaming/entities/Produit.java
+++ 12_TP_eCommerce/src/fr/adaming/entities/Produit.java
@@ -1,5 +1,126 @@
 package fr.adaming.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Définition de la classe persistante Produit
+ */
+
+/**
+ * Annotations pour rendre la classe persistante
+ * Créer une table produits pour enregistrer les attributs
+ */
+@Entity
+@Table(name = "produits")
 public class Produit {
+
+	/**
+	 * Déclaration des attributs
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pro")
+	private Long idProduit;
+
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	private byte[] photo;
+
+	/**
+	 * Déclaration des constructeurs
+	 */
+	public Produit() {
+		super();
+	}
+
+	public Produit(String designation, String description, double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	public Produit(Long idProduit, String designation, String description, double prix, int quantite,
+			boolean selectionne, byte[] photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	/**
+	 * Déclaration des getters et setters
+	 */
+	public Long getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
 
 }
