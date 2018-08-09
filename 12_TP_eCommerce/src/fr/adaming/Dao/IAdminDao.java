28ce03eaa459cecc59c74ade9b878b3e02b6bb5c
Formation <Formation@192.168.1.145>
2018-08-09 11:48:12 +0200

    Valentin : commit dao admin

---
+++ 12_TP_eCommerce/src/fr/adaming/Dao/IAdminDao.java
@@ -0,0 +1,18 @@
+package fr.adaming.Dao;
+
+import javax.ejb.Local;
+
+import fr.adaming.entities.Produit;
+
+@Local
+
+public interface IAdminDao {
+	
+	
+	
+	private Produit addProduit(Admin a);
+	
+	
+	
+
+}
